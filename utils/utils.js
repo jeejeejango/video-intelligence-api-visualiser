55fad2a38fe9fcc875367c7a12dbf37b1f7c3727
Boon Ping Teo <boonping.teo@gmail.com>
2024-12-06 15:13:10 +0800

    update segment time structure

--- utils/utils.js
+++ utils/utils.js
@@ -31,8 +31,12 @@ function nullable_time_offset_to_seconds(time_offset) {
     if (!time_offset)
         return 0
 
-    var seconds = time_offset.seconds || 0
-    seconds += time_offset.nanos / 10e8 || 0
-    return seconds
+    if (typeof(time_offset) == 'object') {
+        var seconds = time_offset.seconds || 0
+        seconds += time_offset.nanos / 10e8 || 0
+        return seconds    
+    } else {
+        return parseFloat(time_offset)
+    }
 }
 
